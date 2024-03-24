5b76605135b50884a875413496a602ce7ea82199
rakeshmani35 <rakeshmani35@gmail.com>
2024-03-24 23:19:08 +0530

    first commit jenkins project

---
+++ src/main/java/com/devops/jenkins/JenkinsCiCdApplication.java
@@ -0,0 +1,22 @@
+package com.devops.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class JenkinsCiCdApplication {
+
+    @GetMapping("/greeting/${name}")
+    public String greeting(@PathVariable String name) {
+        return "Hello " + name + " Congrats to complete CI and CD";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCiCdApplication.class, args);
+    }
+
+}
